70f04ba31d88159a6850810ab2c827ac2be3973a
AbduazimDev <20azimjonmahmudov03@gmail.com>
2023-04-23 10:45:45 +0500

    static files location update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f8&wd3z^ml%diwjavp1f_gvl5cbtct*3exwz*%_)8uz-rp7h62'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -116,7 +116,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'portfoliweb/templates/portfoliweb/assets/im
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfoliweb/templates/portfoliweb/assets'),
+    os.path.join(BASE_DIR, '../static'),
 ]
 
 # Default primary key field type
