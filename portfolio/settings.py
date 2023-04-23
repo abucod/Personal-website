c88ab1d7b9b4f110e3ba488b409dece3ee8ca0d9
AbduazimDev <20azimjonmahmudov03@gmail.com>
2023-04-23 10:17:51 +0500

    Debug : False

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f8&wd3z^ml%diwjavp1f_gvl5cbtct*3exwz*%_)8uz-rp7h62'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
